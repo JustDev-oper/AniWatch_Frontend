07176886b769a2c9a0434fbfbbfbc74ddb4367b0
dead.code <itoppro11@gmail.com>
2025-12-05 19:56:16 +0300

    feat: Сделано плавающее окно и удален ползунок громкости

--- src/pages/AnimeWatchPage.tsx
+++ src/pages/AnimeWatchPage.tsx
@@ -197,7 +197,7 @@ export function AnimeWatchPage() {
             "progress",
             "current-time",
             "mute",
-            "volume",
+            "pip",
             "settings",
             "fullscreen",
         ],
