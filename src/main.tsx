b2e0b182aeda9e0f3bde2e7bf28a4cf919c7305b
dead.code <itoppro11@gmail.com>
2025-12-05 15:01:37 +0300

    Try to fix error

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@ import {createRoot} from "react-dom/client";
 import {BrowserRouter} from "react-router-dom";
 import {App} from "./App";
 import "./static/css/index.css";
-import "./static/css/anime.css"
+import "./static/css/anime.css";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename="/AniWatch_Frontend/">
             <App/>
         </BrowserRouter>
     </React.StrictMode>
