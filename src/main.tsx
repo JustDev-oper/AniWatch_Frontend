1dd7bb39187e32db978187dfbce5af6b2f97b76b
dead.code <itoppro11@gmail.com>
2025-12-05 19:11:40 +0300

    fix: Решил проблему с 404 при обновлении страницы

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {App} from "./App";
 import "./static/css/index.css";
 import "./static/css/anime.css";
@@ -10,8 +10,8 @@ const root = createRoot(container);
 
 root.render(
     <React.StrictMode>
-        <BrowserRouter basename="/AniWatch_Frontend/">
+        <HashRouter>
             <App/>
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>
 );
